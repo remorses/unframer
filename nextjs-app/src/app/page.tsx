1a8b2b34ab582c1d4472277c3d7c17ba98791413
remorses <beats.by.morse@gmail.com>
2023-06-10 15:22:51 +0200

    imported another component

--- nextjs-app/src/app/page.tsx
+++ nextjs-app/src/app/page.tsx
@@ -3,11 +3,16 @@ import 'framer-for-next/styles.css'
 
 import { FontsForComponents, WithFramerBreakpoints } from 'framer-for-next/src'
 import Cultrure from 'https://framer.com/m/cultrure-b10L.js#x'
+import MegaMenu from 'https://framer.com/m/Mega-Menu-2wT3.js@W0zNsrcZ2WAwVuzt0BCl'
 
 export default function Home() {
     return (
-        <div className='bg-white text-gray-800 flex flex-col items-center p-10'>
+        <div className='bg-gray-100 text-gray-800 flex flex-col items-center p-10'>
             <FontsForComponents Components={[Cultrure]} />
+            <div className='z-50 fixed top-12 '>
+                <MegaMenu />
+            </div>
+            <div className="pt-[200px]"></div>
             <div className='max-w-[1200px] w-full'>
                 <WithFramerBreakpoints
                     breakpoints={{
