d440b1c096c3f728b5f044a6a25e682a123dbf2a
remorses <beats.by.morse@gmail.com>
2023-09-20 15:33:32 +0200

    nextjs-app/src/app/page: clean up old code

--- nextjs-app/src/app/page.tsx
+++ nextjs-app/src/app/page.tsx
@@ -1,25 +1,16 @@
 'use client'
-import 'framer-for-next/styles.css'
 
-import Cultrure from 'https://framer.com/m/cultrure-b10L.js#x'
-import Avatar from 'framer-avatar'
-import MegaMenu from 'https://framer.com/m/Mega-Menu-2wT3.js@W0zNsrcZ2WAwVuzt0BCl'
-import LogoTicker from 'https://framer.com/m/Logo-Ticker-1CEq.js@YtVlixDzOkypVBs3Dpav'
 import {
     FramerStyles,
     WithFramerBreakpoints,
 } from 'installable-framer/dist/react'
 import Logos from '@/framer/logos'
 
-console.log('Logos', Logos)
-
 export default function Home() {
     return (
         <div className='bg-gray-100 text-gray-800 flex flex-col items-center p-10'>
-            <FramerStyles Components={[Cultrure]} />
-            <div className='z-50 fixed top-12 '>
-                <MegaMenu />
-            </div>
+            <FramerStyles Components={[Logos]} />
+
             <div className='pt-[500px]'>
                 <WithFramerBreakpoints
                     variants={{
@@ -40,28 +31,6 @@ export default function Home() {
                 {/* <Logos variant='Logo Ticker - M' /> */}
             </div>
             <div className='pt-[200px]'></div>
-            <Avatar radius={10} image={{ src: '/next.svg' }} />
-            <div className='max-w-[1200px] w-full'>
-                <WithFramerBreakpoints
-                    variants={{
-                        Desktop: 'desktop',
-                        Tablet: 'tablet',
-                        Mobile: 'mobile',
-                    }}
-                    Component={Cultrure}
-                    style={{ width: '100%' }}
-                ></WithFramerBreakpoints>
-            </div>
-            <div className='max-w-[1200px] w-full'>
-                <WithFramerBreakpoints
-                    Component={LogoTicker}
-                    variants={{
-                        Desktop: 'Primary',
-                        Tablet: 'Logo Ticker - M',
-                        Mobile: 'Logo Ticker - M',
-                    }}
-                ></WithFramerBreakpoints>
-            </div>
         </div>
     )
 }
