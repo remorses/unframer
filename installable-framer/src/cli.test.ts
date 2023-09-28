d937d4c2ef8d6370abd68d899b3d46afbdaaf3f9
remorses <beats.by.morse@gmail.com>
2023-09-28 14:41:04 +0200

    installable-framer/src/cli.test: add test

--- installable-framer/src/cli.test.ts
+++ installable-framer/src/cli.test.ts
@@ -21,11 +21,28 @@ test(
     1000 * 10,
 )
 test(
+    'issue #1',
+    async () => {
+        const tempFolder = tmp.dirSync({ unsafeCleanup: true }).name
+        console.log('tempFolder', tempFolder)
+        const url = 'https://framer.com/m/Item-Qetw.js@vUDyI0yvPLONiBDf8Kzw'
+        const { types } = await bundle({
+            url,
+            cwd: tempFolder,
+            name: 'item',
+        })
+        console.log(types)
+        console.log(tempFolder)
+    },
+    1000 * 10,
+)
+test(
     'bundle ticker variant',
     async () => {
         const tempFolder = tmp.dirSync({ unsafeCleanup: true }).name
         console.log('tempFolder', tempFolder)
-        const url = 'https://framer.com/m/Brand-Logo-Ticker-Uc8E.js@WLfLN2D3C6m9DWtZu0ci'
+        const url =
+            'https://framer.com/m/Brand-Logo-Ticker-Uc8E.js@WLfLN2D3C6m9DWtZu0ci'
         const { types } = await bundle({
             url,
             cwd: tempFolder,
