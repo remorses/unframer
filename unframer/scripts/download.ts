98dc83c9393049071a59c504f2ee3909970169ea
remorses <beats.by.morse@gmail.com>
2024-03-20 16:25:39 +0100

    unframer/scripts/download:

--- unframer/scripts/download.ts
+++ unframer/scripts/download.ts
@@ -12,7 +12,12 @@ import {
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
+const session = process.env.FRAMER_SESSION
+
 export async function main({ framerUrl, framerTypesUrl }) {
+    const { src } = await getLatestFramerScriptSrc({ session })
+    console.log('src', src)
+    return
     let out = path.resolve(__dirname, '../framer-fixed/dist')
     out = path.resolve(out)
     fs.mkdirSync(path.resolve(out), { recursive: true })
@@ -103,4 +108,46 @@ function extractFramerMotionVersion(code: string) {
         return ''
     }
     return match[1]
+}
+
+async function getLatestFramerScriptSrc({ session }) {
+    const res = await fetch(
+        'https://framer.com/projects/unframer--MOHUmEgItazhBLBtW6H0-adT3u?node=augiA20Il',
+        {
+            headers: {
+                accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'accept-language': 'it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7',
+                'cache-control': 'max-age=0',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'same-origin',
+                'sec-fetch-site': 'same-origin',
+                'upgrade-insecure-requests': '1',
+                cookie: `session=${session};`,
+            },
+            referrer:
+                'https://framer.com/projects/?teamId=68b7d393-dc1e-4609-bf20-92f559ab1ce6',
+            referrerPolicy: 'strict-origin-when-cross-origin',
+            body: null,
+            method: 'GET',
+            mode: 'cors',
+            credentials: 'include',
+        },
+    )
+    const html = await res.text()
+    // console.log('html', html)
+    // extract src from this code:
+    // <script>
+    //     window.exportAssets = Object.freeze({
+    //         library: "https://app.framerstatic.com/framer.YTPROCQS.js",
+    //         framerMotion: "https://app.framerstatic.com/framer-motion.5PJAF455.js",
+    //     })
+    // </script>
+    const match = html.match(
+        /window.exportAssets = Object.freeze\({\s*library: "([^"]+)"/,
+    )
+    if (!match) {
+        throw new Error('Failed to extract framer script src')
+    }
+    const src = match[1]
+    return { src }
 }
