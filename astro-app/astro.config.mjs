61162c96505a31cb4b7c7b89fe604b46a8444aee
remorses <beats.by.morse@gmail.com>
2025-01-30 10:42:26 +0100

    adding framer components to astro, works

--- astro-app/astro.config.mjs
+++ astro-app/astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
-});
+    integrations: [react({ experimentalReactChildren: true }), tailwind()],
+})
