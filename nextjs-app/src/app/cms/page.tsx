14b7a3e8b00eefe24678b1934563d317bdaccce0
remorses <beats.by.morse@gmail.com>
2025-02-04 17:30:30 +0100

    tried adding new cms items

---
+++ nextjs-app/src/app/cms/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import DocsCmsWithFiltersFramerComponent from '@/framer-simplicity/docs-cms-with-filters'
+import '@/framer-simplicity/styles.css'
+
+export default function Page() {
+    return <Home />
+}
+
+function Home() {
+    return (
+        <div className='bg-black flex flex-col items-center'>
+            <DocsCmsWithFiltersFramerComponent />
+
+        </div>
+    )
+}
