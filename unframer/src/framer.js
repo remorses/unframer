ea1ed9e73e2de83c54ee8027f857fc579ad4c20c
remorses <31321188+remorses@users.noreply.github.com>
2025-08-06 12:48:30 +0000

    New Framer Release

--- unframer/src/framer.js
+++ unframer/src/framer.js
@@ -11214,7 +11214,7 @@ function stagger(duration = 0.1, {
   };
 }
 
-// /:https://app.framerstatic.com/framer.JQQVEEM4.mjs
+// /:https://app.framerstatic.com/framer.FPGXVICM.mjs
 import { lazy as ReactLazy, } from 'react';
 import React4 from 'react';
 import { startTransition as startTransition2, } from 'react';
